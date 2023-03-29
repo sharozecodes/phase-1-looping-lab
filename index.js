ec8e2c8ebcaa0d3deb5767f240f39cb14ab1c5d2
Sharoze Archer <sharoze.archer@gmail.com>
2023-03-29 18:15:38 -0400

    Complete Lab

---
+++ index.js
@@ -0,0 +1,27 @@
+// Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(names, eventName){
+    const messages = [];
+    for (let i = 0; i < names.length; i++){
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    
+    return messages;
+}
+
+function countDown(number){
+
+    while (number >= 0){
+        console.log(number);
+        number--;
+    }
+}
